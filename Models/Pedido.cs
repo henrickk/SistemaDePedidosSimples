fe77218c66e6e392954a351571279dda3b0f5a36
Henrick Adrian <henrick.adrian4@gmail.com>
2025-04-01 23:07:29 -0300

    Add project files.

---
+++ Models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaDePedidosSimples.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string? Cliente { get; set; }
+        public DateTime DataPedido { get; set; }
+        public decimal Total { get; set; }
+
+        //relacionamento com PedidoItem
+        public List<PedidoItem> Itens { get; set; } = new List<PedidoItem>();
+    }
+}
---
+++ Models/PedidoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaDePedidosSimples.Models
+{
+    public class PedidoItem
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public string? Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+
+        //relacionamento com Pedido
+        public Pedido Pedido { get; set; }
+    }
+}
